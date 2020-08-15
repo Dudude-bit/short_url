775951bef2161ca9b9a9d2784a81b00dbdbd0871
Dudude <kirillinyakin@yandex.ru>
2020-08-15 19:43:30 +0300

    fixes

--- api/permission_classes.py
+++ api/permission_classes.py
@@ -5,3 +5,9 @@ class IsOwnerOrAnonymousUser(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return request.user == obj.user or not obj.user
+
+
+class IsOwner(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return obj.user == request.user
