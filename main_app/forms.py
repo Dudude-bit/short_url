31d91a4ee446ac935cdf7d468c59d5ac26cf7573
Dudude <kirillinyakin@yandex.ru>
2020-08-17 01:15:53 +0300

    fixes

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,19 +5,22 @@ from django.contrib.auth.forms import UsernameField
 from django.core.validators import MinLengthValidator
 from .models import URLModel
 
+
 class UserRegistrationForm(forms.ModelForm):
     email = forms.EmailField()
-    password1 = forms.CharField(min_length=8, label='Пароль', widget=forms.PasswordInput, validators=[
-        MinLengthValidator(8, message='Ваш пароль должен быть длиннее 8 символов'), ])
-    password2 = forms.CharField(min_length=8, label='Повторите пароль', widget=forms.PasswordInput, validators=[
-        MinLengthValidator(8, message='Ваш пароль должен быть длиннее 8 символов'), ])
+    username = UsernameField(min_length=1, max_length=255)
+    password1 = forms.CharField(min_length=8, label='Пароль', widget=forms.PasswordInput, error_messages={
+        'min_length': 'Ваш пароль слишком короткий'
+    })
+    password2 = forms.CharField(min_length=8, label='Повторите пароль', widget=forms.PasswordInput, error_messages={
+        'min_length': 'Ваш пароль слишком короткий'
+    })
 
     class Meta:
         model = User
         fields = ('username',)
-        field_classes = {'username': UsernameField}
 
-    def clean_password2(self):
+    def clean_password1(self):
         password1 = self.cleaned_data['password1']
         password2 = self.cleaned_data['password2']
         if password1 != password2:
